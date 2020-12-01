35972f897cb2eab7c3d3684a5ff02353c341f2c3
Anders429 <anders.stefan.evensen@gmail.com>
2020-12-01 08:15:31 -0800

    Add documentation to util.

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,3 +1,8 @@
+/// Utility functions for Advent of Code 2020.
+///
+/// These are functions that can likely be used across multiple days. Having them bundled in this
+/// crate prevents having to rewrite the same generic code for each day, saving time.
+
 use std::{
     fmt::Debug,
     fs::File,
@@ -5,6 +10,19 @@ use std::{
     str::FromStr,
 };
 
+/// Read limes from an input file.
+///
+/// Reads lines from the input file `file_name`. Each line is parsed as `T`, where `T` imlements
+/// `FromStr`.
+///
+/// Example usage:
+///
+/// ```
+/// use util::read_input;
+///
+/// // See `int_input` file, which has the numbers 1, 2, 3, and 4 separated by newline characters.
+/// let input = read_input::<u8>("int_input");
+/// assert_eq!(input.collect::<Vec<u8>>(), vec!(1, 2, 3, 4));
 pub fn read_input<T>(file_name: &str) -> impl Iterator<Item = T>
 where
     T: FromStr,
