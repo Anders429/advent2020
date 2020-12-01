286a256ae2a6f02eaebdf638a41957641b7dc787
Anders429 <anders.stefan.evensen@gmail.com>
2020-11-30 21:26:46 -0800

    Setting up a frame to solve the probkem in.

--- day1-silver/src/main.rs
+++ day1-silver/src/main.rs
@@ -1,3 +1,32 @@
+use std::io::BufRead;
+
+fn read_input<T>(file_name: &str) -> impl Iterator<Item = T> 
+where T: std::str::FromStr, <T as std::str::FromStr>::Err : std::fmt::Debug {
+    let file = std::fs::File::open(file_name).unwrap();
+    let reader = std::io::BufReader::new(file);
+    reader.lines().map(|line| line.unwrap()).map(|line| line.parse::<T>().unwrap())
+}
+
+fn solve(input: &[usize]) -> usize {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let input = read_input::<usize>(&args[1]).collect::<Vec<usize>>();
+
+    println!("{}", solve(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solve;
+
+    #[test]
+    fn example() {
+        let input = &[1721, 979, 366, 299, 675, 1456];
+
+        assert_eq!(solve(input), 514575)
+    }
 }
