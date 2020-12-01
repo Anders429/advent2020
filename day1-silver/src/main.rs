6879249ade92b365ccdb4792121120843756ba62
Anders429 <anders.stefan.evensen@gmail.com>
2020-11-30 21:56:48 -0800

    Finished day 1.

--- day1-silver/src/main.rs
+++ day1-silver/src/main.rs
@@ -13,7 +13,45 @@ where
         .map(|line| line.parse::<T>().unwrap())
 }
 
-fn solve(input: &[usize]) -> usize {
+fn solve(input: &[usize], target: usize) -> Option<(usize, usize)> {
+    let mut low_index = 0;
+    let mut high_index = input.len() - 1;
+
+    loop {
+        if high_index <= low_index {
+            return None;
+        }
+
+        let low = input[low_index];
+        let high = input[high_index];
+
+        if low + high < target {
+            low_index += 1;
+        } else if low + high > target {
+            high_index -= 1;
+        } else {
+            return Some((low, high))
+        }
+    }
+}
+
+fn solve_2(input: &[usize]) -> usize {
+    let mut values = input.iter().map(|val| *val).collect::<Vec<usize>>();
+    values.sort_unstable();
+    let (low, high) = solve(input, 2020).unwrap();
+    low * high
+}
+
+fn solve_3(input: &[usize]) -> usize {
+    let mut values = input.iter().map(|val| *val).collect::<Vec<usize>>();
+    values.sort_unstable();
+    for (i, val) in values.iter().enumerate() {
+        if let Some((low, high)) = solve(&values[i..], 2020 - val) {
+            if low + high + val == 2020 {
+                return low * high * val;
+            }
+        }
+    }
     0
 }
 
@@ -22,17 +60,25 @@ fn main() {
 
     let input = read_input::<usize>(&args[1]).collect::<Vec<usize>>();
 
-    println!("{}", solve(&input));
+    //println!("{}", solve_2(&input));
+    println!("{}", solve_3(&input));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solve;
+    use crate::{solve_2, solve_3};
 
     #[test]
-    fn example() {
+    fn example_2() {
         let input = &[1721, 979, 366, 299, 675, 1456];
 
-        assert_eq!(solve(input), 514575)
+        assert_eq!(solve_2(input), 514579)
+    }
+
+    #[test]
+    fn example_3() {
+        let input = &[1721, 979, 366, 299, 675, 1456];
+
+        assert_eq!(solve_3(input), 241861950)
     }
 }
