f5e64988143512811b9c790794572ccb5b04fe25
Anders429 <anders.stefan.evensen@gmail.com>
2020-12-01 08:37:22 -0800

    Added test cases for util.

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -22,16 +22,44 @@ use std::{
 ///
 /// // See `int_input` file, which has the numbers 1, 2, 3, and 4 separated by newline characters.
 /// let input = read_input::<u8>("int_input");
-/// assert_eq!(input.collect::<Vec<u8>>(), vec!(1, 2, 3, 4));
+/// assert_eq!(input.collect::<Vec<u8>>(), vec![1, 2, 3, 4]);
 pub fn read_input<T>(file_name: &str) -> impl Iterator<Item = T>
 where
     T: FromStr,
     <T as FromStr>::Err: Debug,
 {
-    let file = File::open(file_name).unwrap();
+    let file = File::open(file_name).expect(&format!("Could not open file {}", file_name));
     let reader = BufReader::new(file);
     reader
         .lines()
-        .map(|line| line.unwrap())
-        .map(|line| line.parse::<T>().unwrap())
+        .map(|line| line.expect("Could not read line"))
+        .map(|line| line.parse::<T>().expect(&format!("Could not parse line {}", line)))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::read_input;
+
+    #[test]
+    fn read_input_integer() {
+        assert_eq!(read_input::<usize>("int_input").collect::<Vec<usize>>(), vec![1,2,3,4]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn no_file() {
+        let _ = read_input::<usize>("non_existant_file");
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_utf8() {
+        let _ = read_input::<String>("invalid_input").collect::<Vec<String>>();
+    }
+
+    #[test]
+    #[should_panic]
+    fn could_not_parse() {
+        let _ = read_input::<usize>("float_input").collect::<Vec<usize>>();
+    }
 }
