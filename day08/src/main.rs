7e23f790114a73e4d41c3175297cc00c0124763c
Anders429 <anders.stefan.evensen@gmail.com>
2020-12-07 21:42:29 -0700

    Initial day 8.

---
+++ day08/src/main.rs
@@ -0,0 +1,12 @@
+use util::read_input;
+
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let input = read_input::<String>(&args[1]).collect::<Vec<String>>();
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+}
