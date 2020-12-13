52c06ee2c4d00f6242c727c8ef5752a78770da87
Anders429 <anders.stefan.evensen@gmail.com>
2020-12-13 00:03:30 -0700

    Finished day 13.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,201 @@
+use std::str::FromStr;
+use util::read_input;
+
+enum Bus {
+    OutOfService,
+    InService(usize),
+}
+
+impl FromStr for Bus {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match usize::from_str_radix(s, 10) {
+            Ok(val) => Ok(Bus::InService(val)),
+            Err(_) => Ok(Bus::OutOfService),
+        }
+    }
+}
+
+fn parse_input(input: &[String]) -> (usize, Box<[Bus]>) {
+    let time = usize::from_str_radix(&input[0], 10).unwrap();
+    let busses = input[1]
+        .split(',')
+        .map(|val| Bus::from_str(val).unwrap())
+        .collect::<Vec<_>>()
+        .into_boxed_slice();
+
+    (time, busses)
+}
+
+fn earliest(time: usize, busses: &[Bus]) -> usize {
+    let mut best_diff = usize::MAX;
+    let mut best_id = 0;
+
+    for (i, bus) in busses.iter().enumerate() {
+        match bus {
+            Bus::InService(val) => {
+                let diff = *val - (time % *val);
+                println!("{} {}", *val, time % *val);
+                if diff < best_diff {
+                    best_diff = diff;
+                    best_id = *val;
+                }
+            }
+            _ => {}
+        }
+    }
+
+    println!("{} {}", best_diff, best_id);
+
+    best_diff * best_id
+}
+
+fn chinese_remainder_theorem(busses: &[Bus]) -> usize {
+    let mut operating = busses
+        .iter()
+        .enumerate()
+        .filter(|(_, bus)| matches!(bus, Bus::InService(_)))
+        .map(|(i, bus)| {
+            if let Bus::InService(val) = bus {
+                (i, *val)
+            } else {
+                (i, 0)
+            }
+        })
+        .map(|(a, n)| (a % n, n))
+        .collect::<Vec<_>>();
+
+    let mut x = operating[0].0;
+    let mut N = operating[0].1;
+
+        // println!("ai={} ni={} ai+1={} ni+1={} x={} N={}", x, N, operating[1].0, operating[1].1, x, N);
+
+    for i in 0..(operating.len() - 1) {
+        while x % operating[i + 1].1 != (operating[i+1].1 - operating[i + 1].0) || x < operating[i+1].1 {
+            x += N;
+        }
+        N *= operating[i+1].1;
+
+        println!("ai={} ni={} ai+1={} ni+1={} x={} N={}", operating[i].0, operating[i].1, operating[i+1].0, operating[i+1].1, x, N);
+    }
+
+    x
+}
+
+fn subsequent(busses: &[Bus]) -> usize {
+    let mut x = 0;
+    let mut n = match busses[0] {
+        Bus::InService(val) => val,
+        _ => 0,
+    };
+    for (i, val) in busses[1..busses.len()]
+    .iter()
+    .enumerate()
+    .filter(|(i, bus)| matches!(bus, Bus::InService(_)))
+    .map(|(i, bus)| if let Bus::InService(val) = bus {(i, *val)} else {(i,0)}) {
+        println!("{} {} {}", x, n, val);
+        
+        let mut m = 0;
+        loop {
+            if x + (m * n) % val == i {
+                x += m * n;
+                n *= val;
+                break;
+            }
+            m += 1;
+        }
+
+            
+    }
+
+    x
+}
+
+fn subsequent2(busses: &[Bus]) -> usize {
+    let operating = busses
+        .iter()
+        .enumerate()
+        .filter(|(_, bus)| matches!(bus, Bus::InService(_)))
+        .map(|(i, bus)| {
+            if let Bus::InService(val) = bus {
+                (i, *val)
+            } else {
+                (i, 0)
+            }
+        })
+        .collect::<Vec<_>>();
+
+
+
+    let mut diff_found = true;
+    let mut vals = operating
+        .iter()
+        .cloned()
+        .map(|(_, val)| val)
+        .collect::<Vec<_>>();
+    let mut start = vals[0];
+    loop {
+        if !diff_found {
+            break;
+        }
+        diff_found = false;
+        for i in 0..vals.len() {
+            if vals[i] < start {
+                vals[i] = operating[i].1 - (start % operating[i].1);
+                if i == 0 {
+                    start = vals[i];
+                }
+            }
+            if (vals[i] - operating[i].0) % operating[0].1 != 0 {
+                diff_found = true;
+            } 
+        }
+    }
+
+    start
+}
+
+// fn subsequent(busses: &[Bus]) -> usize {
+//     let mut result = 0;
+
+//     let mut mult = 0;
+//     let mut offset = 0;
+
+//     for bus in busses {
+//         match bus {
+//             Bus::InService(val) => {
+//                 if offset == 0 {
+//                     // Initialize.
+//                     mult = *val;
+//                 } else {
+//                     let mut x = 1;
+//                     loop {
+//                         if (*val * x + offset) % mult == 0 {
+//                             break;
+//                         }
+//                         x += 1;
+//                     }
+//                     mult *= *val;
+//                     result = *val * x - offset;
+//                 }
+//             },
+//             _ => {
+//             },
+//         }
+//         offset += 1;
+//     }
+
+//     result
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let input = read_input::<String>(&args[1]).collect::<Vec<String>>();
+
+    let (time, busses) = parse_input(&input);
+
+    println!("{}\n", earliest(time, &busses));
+    println!("{}", chinese_remainder_theorem(&busses));
 }
